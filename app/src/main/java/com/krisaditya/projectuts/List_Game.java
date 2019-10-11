c3accdba09dd1bcc70c1e43c818b9d42c87ce957
dcopetz <krisaditya11@gmail.com>
2019-10-11 12:50:53 +0700

    Design Layout menu

---
+++ app/src/main/java/com/krisaditya/projectuts/List_Game.java
@@ -0,0 +1,14 @@
+package com.krisaditya.projectuts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class List_Game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list__game);
+    }
+}
