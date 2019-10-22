2ed0cd33042e1a7f04c38a119561a8534f2aad3a
dcopetz <krisaditya11@gmail.com>
2019-10-23 03:30:08 +0700

    membuat Splash Screen

---
+++ app/src/main/java/com/krisaditya/projectuts/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.krisaditya.projectuts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        },5000);
+    }
+}
